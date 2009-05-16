9c5119ae171ba8621c49dda8cd1b491a6cb8cbb9
David Ellefsen <davidellefsen@gmail.com>
2009-05-16 19:11:40 +0200

    Initial Import

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PDFSlide
+//
+//  Created by David on 2009/05/12.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
