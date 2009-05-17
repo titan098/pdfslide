30c2bdee05abe64e23b1a6d022289d022e3ea6b7
David Ellefsen <davidellefsen@gmail.com>
2009-05-17 12:32:22 +0200

    Adding Slide Class

---
+++ Slide.h
@@ -0,0 +1,16 @@
+//
+//  Slide.h
+//  PDFSlide
+//
+//  Created by David on 2009/05/17.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Slide : NSObject {
+
+}
+
+@end
---
+++ Slide.m
@@ -0,0 +1,14 @@
+//
+//  Slide.m
+//  PDFSlide
+//
+//  Created by David on 2009/05/17.
+//  Copyright 2009 __MyCompanyName__. All rights reserved.
+//
+
+#import "Slide.h"
+
+
+@implementation Slide
+
+@end
