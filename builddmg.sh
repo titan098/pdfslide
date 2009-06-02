e6605f509333cb603f959b573a1b455ea1cd04b4
David Ellefsen <davidellefsen@gmail.com>
2009-06-02 22:50:20 +0200

    modified builddmg.sh

--- builddmg.sh
+++ builddmg.sh
@@ -4,13 +4,38 @@ APPNAME=PDFSlide
 VERSION=0.1
 DIST=universal
 DISTTMP=$APPNAME
+SRCPATH=`pwd`
+SRCDIR=`basename $SRCPATH`
+GIT=/opt/local/bin/git
 
-rm -rf dist
-mkdir dist
+rm -rf /tmp/dist
+mkdir /tmp/dist
+
+#create the source archive
+cd /tmp
+rm -rf /tmp/$SRCDIR
+$GIT clone $SRCPATH $SRCDIR
+cd /tmp/$SRCDIR
+$GIT checkout $VERSION
+#strip cloned repo out of tmp
+rm -rf .git
+
 mkdir $DISTTMP
+#build the release
+xcodebuild -configuration Release
 
+#copy files for disk image creating
 cp -r build/Release/PDFSlide.app $DISTTMP/
 cp COPYING $DISTTMP/
 
-hdiutil create -srcfolder $DISTTMP dist/$APPNAME.dmg
-rm -rf $DISTTMP
+#create the disk image 
+hdiutil create -srcfolder $DISTTMP /tmp/dist/$APPNAME-$VERSION.dmg
+#rm -rf $DISTTMP
+rm -rf build
+
+#create the source archive
+cd /tmp
+tar cfz /tmp/dist/$APPNAME-$VERSION-src.tar.gz $SRCDIR
+
+#copy /tmp/dist back to srcdir
+cp -R dist $SRCPATH
