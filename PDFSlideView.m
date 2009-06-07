149811cc64cd72c3f9cb1986339d64ff97de99c1
David Ellefsen <davidellefsen@gmail.com>
2009-06-07 11:52:09 +0200

    Updated Copyright infomation on source files

--- PDFSlideView.m
+++ PDFSlideView.m
@@ -7,6 +7,21 @@
 //
 //	Control the display of a PDF file using a custom panel
 //
+// This file is part of PDFSlide.
+//
+// PDFSlide is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// PDFSlide is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with PDFSlide.  If not, see <http://www.gnu.org/licenses/>.
+//
 
 #import "PDFSlideView.h"
 
